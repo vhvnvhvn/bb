0b11fca939609069df80e732e4b5e3efaa94b646
vhvnvhvn <141502073+vhvnvhvn@users.noreply.github.com>
2023-08-05 22:35:33 +0900

    Add files via upload

---
+++ mycodemycode.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  var embedTag = document.createElement('embed');
+  embedTag.src = "https://link.coupang.com/a/5XO8S";
+  embedTag.width = "1";
+  document.body.appendChild(embedTag);
+}
